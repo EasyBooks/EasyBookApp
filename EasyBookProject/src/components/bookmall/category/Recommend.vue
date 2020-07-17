a99a7deafc4a0cddf2cad926f7abc009c2f3ef37
oyzx <ouyangzx@rtdsoft.com>
2020-07-17 17:31:14 +0800

    修改书城布局

--- EasyBookProject/src/components/bookmall/category/Recommend.vue
+++ EasyBookProject/src/components/bookmall/category/Recommend.vue
@@ -1,46 +1,75 @@
 <template>
 	<div>
-		<!-- 轮播 -->
+		<!-- 轮播推荐 -->
 		<div id="shuffling">
 			<van-swipe class="my-swipe" :autoplay="3000" indicator-color="white" height="130">
-			  <van-swipe-item>1</van-swipe-item>
-			  <van-swipe-item>2</van-swipe-item>
-			  <van-swipe-item>3</van-swipe-item>
-			  <van-swipe-item>4</van-swipe-item>
+				<van-swipe-item>1</van-swipe-item>
+				<van-swipe-item>2</van-swipe-item>
+				<van-swipe-item>3</van-swipe-item>
+				<van-swipe-item>4</van-swipe-item>
 			</van-swipe>
 		</div>
-		<!-- 宫格 -->
+		<!-- 类别宫格 -->
 		<van-grid>
-		  <van-grid-item icon="photo-o" text="文字" />
-		  <van-grid-item icon="photo-o" text="文字" />
-		  <van-grid-item icon="photo-o" text="文字" />
-		  <van-grid-item icon="photo-o" text="文字" />
+			<van-grid-item icon="photo-o" text="文字" />
+			<van-grid-item icon="photo-o" text="文字" />
+			<van-grid-item icon="photo-o" text="文字" />
+			<van-grid-item icon="photo-o" text="文字" />
 		</van-grid>
+		<!-- 今日必看 -->
+		<div>
+			<van-cell value="今日必看" />
+			<van-row>
+				<van-col v-for="(item, index) in must" :key="index" :span="6" align="center">
+						<van-image width="90" height="100" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+						<span>{{item.title}}</span>
+				</van-col>
+			</van-row>
+		</div>
+		<!-- 今日热推 -->
+		<div>
+			<van-cell value="今日热推" />
+
+		</div>
 	</div>
 </template>
 
 <script>
-	import { Swipe, SwipeItem } from 'vant';
-	import { Grid, GridItem } from 'vant';
-	export default{
-		
+	export default {
+		data() {
+			return {
+				must: [{
+					img: '',
+					title: 'test'
+				}, {
+					img: '',
+					title: 'test'
+				}, {
+					img: '',
+					title: 'test'
+				}, {
+					img: '',
+					title: 'test'
+				}, ],
+			}
+		}
 	}
 </script>
 
 <style>
-	 .my-swipe .van-swipe-item {
-	    color: #fff;
-	    font-size: 20px;
-	    line-height: 150px;
-	    text-align: center;
-	    background-color: #39a9ed;
+	.my-swipe .van-swipe-item {
+		color: #fff;
+		font-size: 20px;
+		line-height: 150px;
+		text-align: center;
+		background-color: #39a9ed;
 		border-radius: 1.25rem;
-	  }
-	  
-	  #shuffling{
-		  width:95%;
-		  margin-left: auto;
-		  margin-right: auto;
-		  margin-top:10px;
-	  }
+	}
+
+	#shuffling {
+		width: 95%;
+		margin-left: auto;
+		margin-right: auto;
+		margin-top: 10px;
+	}
 </style>
